403134509845372395cecc45c19a6220be9bdf3f
chika <chikayoshida0902@gmail.com>
2025-10-19 14:21:36 +0900

    feat: add favicon

--- tsundoku-tama/app/layout.tsx
+++ tsundoku-tama/app/layout.tsx
@@ -1,28 +1,31 @@
-import type { Metadata } from 'next'
-import { Geist, Geist_Mono } from 'next/font/google'
-import { Analytics } from '@vercel/analytics/next'
-import './globals.css'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
+import "./globals.css";
 
-const _geist = Geist({ subsets: ["latin"] });
-const _geistMono = Geist_Mono({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
-}
+  title: "りーどみー",
+  description: "Created with v0",
+  generator: "v0.app",
+  icons: {
+    icon: "../public/read_me_logo.png",
+  },
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`font-sans antialiased`}>
+    <html lang="ja">
+      <body className={`${inter.className} antialiased`}>
+        {" "}
         {children}
         <Analytics />
       </body>
     </html>
-  )
+  );
 }
